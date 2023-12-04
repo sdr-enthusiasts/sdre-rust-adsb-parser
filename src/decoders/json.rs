b1b45f2f8c2278087f538fb9f661509ce58390b0
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2023-12-03 19:53:59 -0700

    json changes

--- src/decoders/json.rs
+++ src/decoders/json.rs
@@ -54,46 +54,6 @@ pub trait NewJSONMessage {
     fn to_json(&self) -> MessageResult<JSONMessage>;
 }
 
-pub trait NewAircraftJSONMessage {
-    fn to_aircraft_json(&self) -> MessageResult<AircraftJSON>;
-}
-
-impl NewAircraftJSONMessage for String {
-    fn to_aircraft_json(&self) -> MessageResult<AircraftJSON> {
-        match serde_json::from_str(self) {
-            Ok(v) => Ok(v),
-            Err(e) => Err(e.into()),
-        }
-    }
-}
-
-impl NewAircraftJSONMessage for str {
-    fn to_aircraft_json(&self) -> MessageResult<AircraftJSON> {
-        match serde_json::from_str(self) {
-            Ok(v) => Ok(v),
-            Err(e) => Err(e.into()),
-        }
-    }
-}
-
-impl NewAircraftJSONMessage for Vec<u8> {
-    fn to_aircraft_json(&self) -> MessageResult<AircraftJSON> {
-        match serde_json::from_slice(self) {
-            Ok(v) => Ok(v),
-            Err(e) => Err(e.into()),
-        }
-    }
-}
-
-impl NewAircraftJSONMessage for &Vec<u8> {
-    fn to_aircraft_json(&self) -> MessageResult<AircraftJSON> {
-        match serde_json::from_slice(self) {
-            Ok(v) => Ok(v),
-            Err(e) => Err(e.into()),
-        }
-    }
-}
-
 /// Implementing `.to_json()` for the type `String`.
 ///
 /// This does not consume the `String`.
@@ -432,7 +392,10 @@ pub struct JSONMessage {
     /// Ground speed in knots.
     #[serde(skip_serializing_if = "Option::is_none", rename = "gs")]
     pub ground_speed: Option<Speed>,
-    /// Geometric Vertical Accuracy (2.2.3.2.7.2.8)
+    /// Indicated Air speed.
+    // TODO: what is the source of this?
+    #[serde(skip_serializing_if = "Option::is_none", rename = "ias")]
+    indicator_air_speed: Option<Speed>,
     #[serde(skip_serializing_if = "Option::is_none", rename = "gva")]
     pub geometric_verticle_accuracy: Option<u8>, // FIXME: I doubt this is right
     /// The transponder hex identifier of the aircraft.
@@ -539,83 +502,6 @@ pub struct JSONMessage {
     /// ADS-B Version Number 0, 1, 2 (3-7 are reserved) (2.2.3.2.7.5)
     #[serde(skip_serializing_if = "Option::is_none")]
     pub version: Option<ADSBVersion>,
-}
-
-/// The JSON message readsb provided aircraft.json format.
-/// This file is a list of JSONMessage with some additional metadata provided.
-
-// TODO: When deserializing no planes in this format will include a timestamp field.
-// However, AircraftJSON provides it. We should inject that timestamp field in to the
-// JSONMessage before deserializing it.
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, PartialOrd, Default)]
-pub struct AircraftJSON {
-    pub now: f32,
-    pub messages: i32,
-    pub aircraft: Vec<JSONMessage>,
-}
-
-impl AircraftJSON {
-    /// Converts `AircraftJSON` to `String`.
-    pub fn to_string(&self) -> MessageResult<String> {
-        match serde_json::to_string(self) {
-            Ok(v) => Ok(v),
-            Err(e) => Err(e.into()),
-        }
-    }
-
-    /// Converts `AircraftJSON` to `String` and appends a `\n` to the end.
-    pub fn to_string_newline(&self) -> MessageResult<String> {
-        match serde_json::to_string(self) {
-            Err(to_string_error) => Err(to_string_error.into()),
-            Ok(string) => Ok(format!("{}\n", string)),
-        }
-    }
-
-    /// Converts `AircraftJSON` to a `String` encoded as bytes.
-    ///
-    /// The output is returned as a `Vec<u8>`.
-    pub fn to_bytes(&self) -> MessageResult<Vec<u8>> {
-        match self.to_string() {
-            Err(conversion_failed) => Err(conversion_failed),
-            Ok(string) => Ok(string.into_bytes()),
-        }
-    }
-
-    /// Converts `AircraftJSON` to a `String` terminated with a `\n` and encoded as bytes.
-    ///
-    /// The output is returned as a `Vec<u8>`.
-    pub fn to_bytes_newline(&self) -> MessageResult<Vec<u8>> {
-        match self.to_string_newline() {
-            Err(conversion_failed) => Err(conversion_failed),
-            Ok(string) => Ok(string.into_bytes()),
-        }
-    }
-
-    pub fn pretty_print(&self) -> String {
-        // TODO: output the now and messages field
-
-        let mut output: String = String::new();
-
-        for aircraft in &self.aircraft {
-            output.push_str(&aircraft.pretty_print());
-        }
-
-        output
-    }
-
-    pub fn len(&self) -> usize {
-        self.aircraft.len()
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.aircraft.is_empty()
-    }
-}
-
-impl fmt::Display for AircraftJSON {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.aircraft.len())
-    }
 }
 
 #[cfg(test)]
