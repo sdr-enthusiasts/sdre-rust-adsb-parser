d27caefe99c014604c84e3afec6cdbc67cde2158
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2023-12-03 19:49:35 -0700

    add a bunch of valid tisb types

--- src/decoders/json_types/tisb.rs
+++ src/decoders/json_types/tisb.rs
@@ -12,6 +12,21 @@ use std::fmt;
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, PartialOrd, Default)]
 #[serde(try_from = "String")]
 pub enum TiSB {
+    BaroRate,
+    Callsign,
+    Altitude,
+    AltGeom,
+    GroundSpeed,
+    Track,
+    GeomRate,
+    Latitude,
+    Longitude,
+    NIC,
+    RC, // TODO: rename these
+    NACp,
+    NACv,
+    SIL,
+    SILType,
     #[default]
     None,
 }
@@ -19,6 +34,21 @@ pub enum TiSB {
 impl fmt::Display for TiSB {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
+            TiSB::BaroRate => write!(f, "Baro Rate"),
+            TiSB::Callsign => write!(f, "Callsign"),
+            TiSB::Altitude => write!(f, "Altitude"),
+            TiSB::AltGeom => write!(f, "Altitude Geometric"),
+            TiSB::GroundSpeed => write!(f, "Ground Speed"),
+            TiSB::Track => write!(f, "Track"),
+            TiSB::GeomRate => write!(f, "Geom Rate"),
+            TiSB::Latitude => write!(f, "Latitude"),
+            TiSB::Longitude => write!(f, "Longitude"),
+            TiSB::NIC => write!(f, "NIC"),
+            TiSB::RC => write!(f, "RC"),
+            TiSB::NACp => write!(f, "NACp"),
+            TiSB::NACv => write!(f, "NACv"),
+            TiSB::SIL => write!(f, "SIL"),
+            TiSB::SILType => write!(f, "SIL Type"),
             TiSB::None => write!(f, "None"),
         }
     }
@@ -30,6 +60,21 @@ impl TryFrom<String> for TiSB {
     fn try_from(field: String) -> Result<Self, Self::Error> {
         match field.as_str() {
             "None" => Ok(TiSB::None),
+            "baro_rate" => Ok(TiSB::BaroRate),
+            "callsign" => Ok(TiSB::Callsign),
+            "altitude" => Ok(TiSB::Altitude),
+            "alt_geom" => Ok(TiSB::AltGeom),
+            "gs" => Ok(TiSB::GroundSpeed),
+            "track" => Ok(TiSB::Track),
+            "geom_rate" => Ok(TiSB::GeomRate),
+            "lat" => Ok(TiSB::Latitude),
+            "lon" => Ok(TiSB::Longitude),
+            "nic" => Ok(TiSB::NIC),
+            "rc" => Ok(TiSB::RC),
+            "nac_p" => Ok(TiSB::NACp),
+            "nac_v" => Ok(TiSB::NACv),
+            "sil" => Ok(TiSB::SIL),
+            "sil_type" => Ok(TiSB::SILType),
             _ => Err(format!("Invalid TiSB field: {}", field)),
         }
     }
