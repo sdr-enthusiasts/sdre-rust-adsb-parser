4031a60e1fe03147bdd34e9b3e644694e4d08326
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2024-01-25 17:07:17 -0600

    Clippy pedantic

--- src/decoders/json_types/speed.rs
+++ src/decoders/json_types/speed.rs
@@ -10,7 +10,8 @@ use std::fmt;
 #[derive(Deserialize, Debug, Clone, PartialEq, PartialOrd, Default)]
 #[serde(from = "f32")]
 pub enum Speed {
-    Knots(f32),
+    KnotsAsF32(f32),
+    KnotsAsF64(f64),
     #[default]
     None,
 }
@@ -19,7 +20,8 @@ impl Speed {
     #[must_use]
     pub fn get_speed(&self) -> f64 {
         match self {
-            Self::Knots(speed) => f64::from(*speed),
+            Self::KnotsAsF32(speed) => f64::from(*speed),
+            Self::KnotsAsF64(speed) => *speed,
             Self::None => 0.0,
         }
     }
@@ -31,7 +33,8 @@ impl Serialize for Speed {
         S: serde::Serializer,
     {
         match *self {
-            Speed::Knots(speed) => serializer.serialize_f32(speed),
+            Speed::KnotsAsF32(speed) => serializer.serialize_f32(speed),
+            Speed::KnotsAsF64(speed) => serializer.serialize_f64(speed),
             Speed::None => serializer.serialize_none(),
         }
     }
@@ -39,29 +42,31 @@ impl Serialize for Speed {
 
 impl From<f64> for Speed {
     fn from(speed: f64) -> Self {
-        Self::Knots(speed as f32)
+        Self::KnotsAsF64(speed)
     }
 }
 
 impl From<f32> for Speed {
     fn from(speed: f32) -> Self {
-        Self::Knots(speed)
+        Self::KnotsAsF32(speed)
     }
 }
 
 impl Speed {
     #[must_use]
-    pub fn as_meters(&self) -> f32 {
+    pub fn as_meters(&self) -> f64 {
         match self {
-            Speed::Knots(speed) => *speed * 0.514_444,
+            Speed::KnotsAsF32(speed) => f64::from(*speed) * 0.514_444,
+            Speed::KnotsAsF64(speed) => *speed * 0.514_444,
             Speed::None => 0.0,
         }
     }
 
     #[must_use]
-    pub fn as_knots(&self) -> f32 {
+    pub fn as_knots(&self) -> f64 {
         match self {
-            Speed::Knots(speed) => *speed,
+            Speed::KnotsAsF32(speed) => f64::from(*speed),
+            Speed::KnotsAsF64(speed) => *speed,
             Speed::None => 0.0,
         }
     }
@@ -69,7 +74,8 @@ impl Speed {
     #[must_use]
     pub fn display_as_knots(&self) -> String {
         match self {
-            Speed::Knots(speed) => format!("{speed} knots"),
+            Speed::KnotsAsF32(speed) => format!("{speed} knots"),
+            Speed::KnotsAsF64(speed) => format!("{speed} knots"),
             Speed::None => "None".to_string(),
         }
     }
@@ -77,7 +83,8 @@ impl Speed {
     #[must_use]
     pub fn display_as_meters(&self) -> String {
         match self {
-            Speed::Knots(speed) => format!("{} m/min", speed * 0.514_444),
+            Speed::KnotsAsF32(speed) => format!("{} m/min", speed * 0.514_444),
+            Speed::KnotsAsF64(speed) => format!("{} m/min", speed * 0.514_444),
             Speed::None => "None".to_string(),
         }
     }
@@ -87,7 +94,8 @@ impl fmt::Display for Speed {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             // cast to u32 to remove the decimal
-            Speed::Knots(speed) => write!(f, "{} knots", *speed),
+            Speed::KnotsAsF32(speed) => write!(f, "{} knots", *speed),
+            Speed::KnotsAsF64(speed) => write!(f, "{} knots", *speed),
             Speed::None => write!(f, "None"),
         }
     }
