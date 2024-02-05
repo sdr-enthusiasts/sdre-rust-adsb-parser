640bb9f9d60e966a6e1416243adf38646a7bec60
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2024-02-05 11:40:13 -0700

    Custom Errors for conversion

---
+++ src/decoders/errors/conversion.rs
@@ -0,0 +1,22 @@
+// Copyright (c) 2024 Frederick Clausen II
+
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+// This file contains the error types for the conversion errors.
+
+use custom_error::custom_error;
+
+custom_error! {pub ConversionError
+    ReservedIsNotZero{source_name: String} = "{source_name} reserved field(s) are not 0",
+    NotImplemented{source_name: String} = "{source_name} is not implemented",
+    LongitudeIsNone = "Calculated longitude is None",
+    LatitudeIsNone = "Calculated latitude is None",
+    UnknownMessageType{message_me: String, me_type: String} = "Unknown message type {message_me} for {me_type}",
+    UnknownADSBVersion = "Unknown ADSB version",
+    UknownCapabilityClass = "Unknown capability class",
+    UnknownOperationalMode = "Unknown operational mode",
+    LatitudeOrLongitudeIsZero{lat: f64, lon: f64} = "Latitude or longitude is 0.0. Latitude: {lat}, Longitude: {lon}. Unable to calculate position",
+    UnableToCalculatePosition = "Unable to calculate position from Even/Odd CPR, supplied reference position, and/or previous aircraft position used as reference position",
+}
